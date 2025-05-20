851ac6c68623daa44706a1b1d45fc1b90dbde68e
Teddy Pillay <tpillay1995@gmail.com>
2025-05-20 16:48:54 -0700

    moved package.json file for render

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+// the pool class is a feature of the pg library that allows us to connect to the database and manages a pool of connections to the database. it is used to create a connection pool and execute queries. By keeping multiple connectsion open it imroves performance. 
 import { Pool } from 'pg';
 
 const pool = new Pool({
